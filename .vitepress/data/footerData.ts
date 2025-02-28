95b244cb3bb1467cbc490049bed2dcffa3125c91
J1nH4ng <j1nh4ng@icloud.com>
2025-02-28 16:40:19 +0800

    feat(footer): 更新底部链接数据，新增友情链接和Github Source部分

--- .vitepress/data/footerData.ts
+++ .vitepress/data/footerData.ts
@@ -6,10 +6,26 @@ export const Footer_Data: FooterData ={
     group: [
         {
             title: '外部链接',
-            icon: 'bx:link',
+            icon: 'bx:link-external',
             color: 'rgba(255, 87, 51, 1)',
             links: [
-                {name: 'Github', link: 'https://github.com/J1nH4ng', icon: 'solar:github-alt'}
+                {name: 'Github', link: 'https://github.com/J1nH4ng', icon: 'solar:github-alt'},
+                {name: 'LeetCode', link: 'https://leetcode.cn', icon: 'https://leetcode.cn/favicon.ico'}
+            ]
+        },
+        {
+            title: '友情链接',
+            icon: 'bx:paper-plane',
+            color: 'rgba(255, 87, 51, 1)',
+            links: [
+                { name: 'Shaw\'s Book', link: 'https://xiaoshebro.github.io/', icon: 'https://xiaoshebro.github.io/logo.svg'}
+            ]
+        },
+        {
+            title: 'Github Source',
+            icon: 'bx:bxl-github',
+            links: [
+                { name: 'Keep', link: 'https://github.com/keephq/keep', icon: 'solar:github-alt'}
             ]
         }
     ]
