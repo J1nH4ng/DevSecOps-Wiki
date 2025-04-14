281b4fc8df7f03782baff64762c3631fc4434dd5
J1nH4ng <j1nh4ng@icloud.com>
2025-04-14 15:53:26 +0800

    fix: 修改底部作者名称

--- .vitepress/data/footerData.ts
+++ .vitepress/data/footerData.ts
@@ -2,7 +2,7 @@ import type { FooterData } from "@theojs/lumen";
 
 export const Footer_Data: FooterData ={
     beian: { icp: '苏 ICP 备 2024149705 号', police: '苏公网安备 32050502012208 号', showIcon: true },
-    author: { name: 'J1nH4ng', link: 'https://www.4r3al.team' },
+    author: { name: 'Team 4r3al', link: 'https://www.4r3al.team' },
     group: [
         {
             title: '外部链接',
